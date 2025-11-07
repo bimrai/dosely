fe97a2d0e40d2de869275d37dee3a4b706f9c4f8
BR-117 <birahoma@outlook.com>
2025-11-07 10:47:10 +0000

    Initial Commit

---
+++ DoselyTests/DoselyTests.swift
@@ -0,0 +1,17 @@
+//
+//  DoselyTests.swift
+//  DoselyTests
+//
+//  Created by Bim Rai on 07/11/2025.
+//
+
+import Testing
+@testable import Dosely
+
+struct DoselyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
